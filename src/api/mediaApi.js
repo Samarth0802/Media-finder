aa4e211804a907bba83e9580a647602f858323ac
Samarth Varshney <samarthvarshney08022005@gmail.com>
2026-01-17 19:49:39 +0530

    commit-env

--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -3,8 +3,9 @@ import axios from 'axios'
 const unsplash_key = import.meta.env.VITE_UNSPLASH_KEY
 const pexels_key = import.meta.env.VITE_PEXELS_KEY
 const giphy_key = import.meta.env.VITE_GIPHY_KEY
+// console.log(JSON.stringify(unsplash_key))
 
-console.log(unsplash_key,pexels_key)
+// console.log(unsplash_key,pexels_key,giphy_key)
 
 export async function getPhotos(query,page=1,per_page=30){
     let response = await axios.get('https://api.unsplash.com/search/photos',{
@@ -14,7 +15,7 @@ export async function getPhotos(query,page=1,per_page=30){
             per_page
         },
         headers:{
-            Authorization: `Client-ID ${unsplash_key}`
+            Authorization:`Client-ID ${unsplash_key}`
         }
     })
     return response.data
@@ -36,12 +37,12 @@ export async function getVideos(query,page=1,per_page=40){
     return response.data
 }
 
-export async function getGif(query,limit=25){
+export async function getGif(query,per_page=25){
     let response = await axios.get('https://api.giphy.com/v1/gifs/search',{
         params:{
-            query,
+            q:query,
             apikey:giphy_key,
-            limit,
+            limit:per_page,
         }
     })
     return response.data
